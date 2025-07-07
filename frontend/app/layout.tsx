5dd72a905a183e7f709a5bbfd27f2d97dab607ac
swe-robertkibet <swe.robertkibet@gmail.com>
2025-07-07 07:48:48 +0300

    Update project title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "sheria-smart",
+  title: "Sheria Smart",
   description: "AI KenyanLegal assistance",
   generator: "sheria-smart",
 };
